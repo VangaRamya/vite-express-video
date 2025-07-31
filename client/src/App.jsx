6ac3211dd010f66f950742de95362f3d750c860d
raju.palumaru <raju.palumaru@brinta.in>
2025-07-31 12:55:27 +0530

    added url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api");
+    const response = await axios.get("https://vite-express-video.onrender.com/api");
     setArray(response.data.fruits);
     console.log(response.data.fruits);
   };
